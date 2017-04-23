ef33b316c78783093a7e496710c5f274f60f438f
ttayaa <26442596@qq.com>
2017-04-23 14:39:03 +0800

    two

---
+++ TTSourceDemo/TTSourceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTSourceDemo
+//
+//  Created by apple on 2017/4/23.
+//  Copyright © 2017年 ttayaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTSourceDemo/TTSourceDemo/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  TTSourceDemo
+//
+//  Created by apple on 2017/4/23.
+//  Copyright © 2017年 ttayaa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.clickSignalName = @"asdasd";
+    
+}
+
+Click_signal(asdasd)
+{
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

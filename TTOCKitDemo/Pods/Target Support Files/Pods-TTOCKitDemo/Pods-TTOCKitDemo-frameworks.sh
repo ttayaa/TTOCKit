b354bbf063a775f683200ab7927385bfb6a87379
ttayaa <26442596@qq.com>
2020-05-29 15:36:36 +0800

    2

--- TTOCKitDemo/Pods/Target Support Files/Pods-TTOCKitDemo/Pods-TTOCKitDemo-frameworks.sh
+++ TTOCKitDemo/Pods/Target Support Files/Pods-TTOCKitDemo/Pods-TTOCKitDemo-frameworks.sh
@@ -198,9 +198,17 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DZNEmptyDataSet/DZNEmptyDataSet.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TTOCKit/TTOCKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DZNEmptyDataSet/DZNEmptyDataSet.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TTOCKit/TTOCKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
---
+++ TTOCKitDemo/Pods/Target Support Files/TTOCKit/TTOCKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TTOCKit : NSObject
+@end
+@implementation PodsDummy_TTOCKit
+@end
